906c36256819cfd316a32f45b705de23b31089c2
hamzashadeez <hahmad1178@gmail.com>
2020-09-26 01:02:59 +0100

    Posting

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -5,13 +5,40 @@ import AddCircleIcon from "@material-ui/icons/AddCircle";
 import { db } from "../../firebase";
 import { IconButton } from "@material-ui/core";
 import PostFeedModal from "../Modals/PostFeedModal";
-import Button from 'react-bootstrap/Button'
+// import { Button } from "react-bootstrap";
+import { Button, TextField } from "@material-ui/core";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    width: "70%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement(document.getElementById("root"));
 
 function Feed() {
   const [feeds, setFeeds] = useState([]);
   const [open, handleClose] = useState(false);
+  var subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [image, setImage] = useState('');
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-  
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = "#f00";
+  }
   useEffect(() => {
     db.collection("feeds").onSnapshot((snapshot) => {
       console.log(snapshot.docs);
@@ -26,7 +53,7 @@ function Feed() {
   return (
     <div className="feed">
       <div className="addicon">
-        <IconButton onClick={() => handleClose(true)}>
+        <IconButton onClick={openModal}>
           <AddCircleIcon color="secondary" style={{ fontSize: 50 }} />
         </IconButton>
       </div>
@@ -41,8 +68,35 @@ function Feed() {
           />
         );
       })}
-      
-      
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 id='modalheader'>Add A Post</h2>
+        {/* <Button id='photo'color='default' variant='contained'>Choose An Image</Button> */}
+        <input color='secondary' id='photo' type='file' onChange={(e)=>setImage(e.target.value)}/>
+        <form>
+          <TextField
+            id="outlined-multiline-static"
+            label="Write Your description here"
+            multiline
+            color='secondary'
+            rows={2}
+            id='textfield'
+            fullWidth
+            // defaultValue="Default Value"
+            variant="outlined"
+          />
+          <div style={{display: 'flex', justifyContent: 'space-around', marginTop:'10px'}}>
+          <Button color='secondary' variant='contained'>SUBMIT</Button>
+          <Button color='default' variant='contained' onClick={closeModal}>CANCEL</Button>
+
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 }
