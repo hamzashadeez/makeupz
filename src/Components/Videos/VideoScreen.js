9e4f01964ae4b557c95250f929c5bdd8838ce955
hamzashadeez <hahmad1178@gmail.com>
2020-10-15 06:34:33 +0100

    stylingl

--- src/Components/Videos/VideoScreen.js
+++ src/Components/Videos/VideoScreen.js
@@ -6,11 +6,11 @@ const VideoScreen = () => {
     return (
         <div className='Video_screen'>
             <div className='alerting'>
-                <h6>Videos a cikin harshen Hausa suna nan take ba da dadewa ba </h6>
+                <h6>Videos a cikin harshen Hausa suna nan tafe ba da dadewa ba </h6>
             </div>
-            <Video src={'https://www.youtube.com/embed/Urhco0_M7iE'} title='Easy Makeup for Beginners' />
             <Video src={"https://www.youtube.com/embed/-k9ZpHLxMoI"} title='Makeup #1'/>
             <Video src={"https://www.youtube.com/embed/Jgpkddk2n60"} title='DETAILED UPDATED EYEBROW TUTORIAL'/>
+            <Video src={'https://www.youtube.com/embed/Urhco0_M7iE'} title='Easy Makeup for Beginners' />
         </div>
     )
 }
